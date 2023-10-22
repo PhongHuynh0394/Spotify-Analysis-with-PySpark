6581fe3ccc82ad6cd54532f9a8e8e7f97d4447fd
PhongHuynh0394 <phonghuynh9403@gmail.com>
2023-10-22 20:00:31 +0700

    Updated runing two sample deployment

--- prefect/flows/main_flow.py
+++ prefect/flows/main_flow.py
@@ -1,11 +1,27 @@
-from prefect import task, flow
+from prefect import flow, serve
 from sample_task import *
 
-@flow(name="hello_flow", log_prints=True) 
+@flow(name="hello flow A", 
+      log_prints=True)
+def flow_A():
+    """This is a sample hello flow A"""
+    print("This is flow A")
+
+
+@flow(name="hello flow B", 
+      log_prints=True) 
 def hello_flow():
-    name = getName()
-    lastName = getLastName()
+    """This is a sample hello flow B"""
+    name = getName() #Run task getName
+    lastName = getLastName() #Run task getLastName
     print(f"Hello {name} {lastName}")
 
+
+
 if __name__ == "__main__":
-    hello_flow.serve(name='test-deployment')
+    hello_flow = hello_flow.to_deployment(name='test-hello-B-deployment',
+                             tags=['sample tag B', 'sample tag other'],
+                             interval=600) # 600 seconds
+    flow_A = flow_A.to_deployment(name='test-hello-A-deployment',
+                             tags=['tag A', 'tag other'])
+    serve(hello_flow, flow_A)
