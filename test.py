cf771b58cc8e5328669687ae20af5f4cb6c625f3
PhongHuynh0394 <phonghuynh9403@gmail.com>
2023-10-27 22:21:55 +0700

    testdb

---
+++ test.py
@@ -0,0 +1,13 @@
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://root:<password>@python.zynpktu.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
