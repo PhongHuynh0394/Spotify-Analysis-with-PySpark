6725e013a7399e560b8d2d68bd6f9888438856cc
PhongHuynh0394 <phonghuynh9403@gmail.com>
2023-10-30 10:16:41 +0700

    modified test mongo

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://root:<password>@python.zynpktu.mongodb.net/?retryWrites=true&w=majority"
+password = 123
+uri = f"mongodb+srv://root:{password}@python.zynpktu.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
+
+db = client.list_database_names()
+
+print('databases:')
+for db_name in db:
+    print(db_name)
 
 # Send a ping to confirm a successful connection
 try:
