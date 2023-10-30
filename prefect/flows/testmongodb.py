e1b77ca034c556d50b6ba648fd7b6a325506b4ad
PhongHuynh0394 <phonghuynh9403@gmail.com>
2023-10-30 11:21:16 +0700

    added new .env mongodb

--- prefect/flows/testmongodb.py
+++ prefect/flows/testmongodb.py
@@ -1,9 +1,15 @@
 from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+user = os.getenv("MONGODB_USER")
+password = os.getenv("MONGODB_PASSWORD")
 
 password = 123
 # public="jiowdstr"
 # private="0b24c5ce-b981-4885-a202-73457caf5925"
-uri = f"mongodb+srv://root:{password}@python.zynpktu.mongodb.net/?retryWrites=true&w=majority"
+uri = f"mongodb+srv://{user}:{password}@python.zynpktu.mongodb.net/?retryWrites=true&w=majority"
 # connection_string = f"mongodb+srv://{private}@clustername.mongodb.net/test?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
